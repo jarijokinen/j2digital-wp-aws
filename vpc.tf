1dfe3c247746e42436231a8dbab6151fb916ecad
Jari Jokinen <jari@j2digital.io>
2024-08-31 19:57:16 +0300

    Create subnets

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,26 @@ resource "aws_internet_gateway" "wp" {
   vpc_id = aws_vpc.wp.id
 }
 
+resource "aws_subnet" "private_a" {
+  vpc_id = aws_vpc.wp.id
+  cidr_block = "10.0.0.0/27"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id = aws_vpc.wp.id
+  cidr_block = "10.0.0.32/27"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id = aws_vpc.wp.id
+  cidr_block = "10.0.0.64/27"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id = aws_vpc.wp.id
+  cidr_block = "10.0.0.96/27"
+  availability_zone = "us-east-1b"
+}
