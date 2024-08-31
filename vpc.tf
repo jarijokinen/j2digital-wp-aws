ab3d9ef5c76752ce7ff59aeca7a3cd3a6aba3f06
Jari Jokinen <jari@j2digital.io>
2024-08-31 22:43:11 +0300

    Fix formatting

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "wp" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block           = "10.0.0.0/24"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 }
 
 resource "aws_internet_gateway" "wp" {
@@ -9,26 +9,26 @@ resource "aws_internet_gateway" "wp" {
 }
 
 resource "aws_subnet" "private_a" {
-  vpc_id = aws_vpc.wp.id
-  cidr_block = "10.0.0.0/27"
+  vpc_id            = aws_vpc.wp.id
+  cidr_block        = "10.0.0.0/27"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "private_b" {
-  vpc_id = aws_vpc.wp.id
-  cidr_block = "10.0.0.32/27"
+  vpc_id            = aws_vpc.wp.id
+  cidr_block        = "10.0.0.32/27"
   availability_zone = "us-east-1b"
 }
 
 resource "aws_subnet" "public_a" {
-  vpc_id = aws_vpc.wp.id
-  cidr_block = "10.0.0.64/27"
+  vpc_id            = aws_vpc.wp.id
+  cidr_block        = "10.0.0.64/27"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id = aws_vpc.wp.id
-  cidr_block = "10.0.0.96/27"
+  vpc_id            = aws_vpc.wp.id
+  cidr_block        = "10.0.0.96/27"
   availability_zone = "us-east-1b"
 }
 
