ac601a448d8ae765b730476b126977577eb50b87
Jari Jokinen <jari@j2digital.io>
2024-09-03 01:16:09 +0300

    Add ecsTaskExecutionRole and ecsTaskExecutionPolicy

--- iam.tf
+++ iam.tf
@@ -23,3 +23,51 @@ resource "aws_iam_instance_profile" "ecs_instance_role" {
   name  = "ecsInstanceRole-profile"
   role  = aws_iam_role.ecs_instance_role.name
 }
+
+# ecsTaskExecutionRole
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = <<-EOT
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "ecs-tasks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+  EOT
+}
+
+resource "aws_iam_policy" "ecs_task_execution" {
+  name        = "ecsTaskExecutionPolicy"
+  description = "Allows ECS tasks to get secrets."
+  policy      = <<-EOT
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "kms:Decrypt",
+            "secretsmanager:GetSecretValue"
+          ],
+          "Resource": [
+            "${aws_kms_key.wp.arn}",
+            "${aws_db_instance.wp.master_user_secret.0.secret_arn}"
+          ]
+        }
+      ]
+    }
+  EOT
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.ecs_task_execution.arn
+}
